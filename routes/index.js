b8749116c84dcb607f0d8d6ce293a996d41926d9
Nick Novello <nicknovello@ymail.com>
2020-07-18 01:36:02 -0400

    added catch-all

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.use('/api/matches', matchRoutes);
 router.use('/api/auth', authRoutes);
 
 router.use('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
 module.exports = router;
